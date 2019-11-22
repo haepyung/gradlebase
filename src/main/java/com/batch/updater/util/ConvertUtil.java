a551003e162cf1ae26642c872ac94d59bc6feab5
haepyung <haepyung88@gmail.com>
2019-11-22 12:59:30 +0900

    # gradlebase

---
+++ src/main/java/com/batch/updater/util/ConvertUtil.java
@@ -0,0 +1,4 @@
+package com.batch.updater.util;
+
+public class ConvertUtil {
+}
---
+++ src/test/java/com/batch/updater/GradlebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
