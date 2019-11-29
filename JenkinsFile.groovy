043528ddb7ddfaf8943803f14038596ac1e5586b
haepyung <haepyung88@gmail.com>
2019-11-29 15:43:48 +0900

    test: jenkins연동
    Change-Id: I2281622d308b0c71505436b8c025727f66e3a150

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -18,7 +18,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo "Build"
-                sh('gradle build')
+                sh('./gradlew build')
             }
         }
     }
