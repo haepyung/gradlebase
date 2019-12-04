449e7e49db2e91bf23dcbdd118e84a7e73759173
haepyung <haepyung88@gmail.com>
2019-12-04 15:12:25 +0900

    test: jenkins docker연동

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -15,6 +15,20 @@ pipeline {
             }
         }
 
+        stage ('Test 3: Master') {
+            when { branch 'origin/master' }
+            steps {
+                echo 'I only execute on the master branch.'
+            }
+        }
+
+        stage ('Test 3: Dev') {
+            when { not { branch 'master' } }
+            steps {
+                echo 'I execute on non-master branches.'
+            }
+        }
+
         stage('[STEP_3] dockerfile build') {
             steps {
                 sh 'docker stop gradlebuild || true && docker rm gradlebuild || true'
