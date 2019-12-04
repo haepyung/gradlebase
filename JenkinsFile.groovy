2d5c947b5a680a8f3cef091684b41e13076fcbce
haepyung <haepyung88@gmail.com>
2019-12-04 15:44:03 +0900

    test: jenkins docker연동

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,43 +1,29 @@
 pipeline {
     agent any
     environment {
-        branch_name = "${env.GIT_BRANCH}"
+        branch_name = "${env.NODE_NAME}"
     }
     stages {
-        stage('[STEP_1] show Infos') {
+        stage('STEP_1:: show Infos') {
             steps {
                 //sh 'printenv' -- 쓸수 있는 전체 정보
                 echo "search branch ID:: ${env.GIT_BRANCH}, Name:: ${branch_name}"
             }
         }
 
-        stage('[STEP_2] gradle build') {
+        stage('STEP_2:: gradle build') {
             steps {
                 sh 'chmod +x ./gradlew'
                 sh './gradlew build'
             }
         }
 
-        stage ('Test 3: Master') {
-            when { branch 'master' }
+        stage('STEP_3:: dockerfile build') {
             steps {
-                echo 'I execute on master branches.'
-            }
-        }
-
-        stage ('Test 3: Dev') {
-            when { not {branch 'deb'} }
-            steps {
-                echo 'no have deb branch!!'
-            }
-        }
-
-        stage('[STEP_3] dockerfile build') {
-            steps {
-                sh 'docker stop gradlebuild || true && docker rm gradlebuild || true'
-                sh 'docker rmi gradletsource || true'
-                sh 'docker build -t gradletsource .'
-                sh 'docker run --name gradlebuild -d -p 8888:8888 gradletsource'
+                sh "docker stop gradlebuild_${branch_name}|| true && docker rm gradlebuild_${branch_name} || true"
+                sh "docker rmi gradletsource_${branch_name} || true"
+                sh "docker build -t gradletsource_${branch_name} ."
+                sh "docker run --name gradlebuild_${branch_name} -d -p 8888:8888 gradletsource"
             }
         }
     }
